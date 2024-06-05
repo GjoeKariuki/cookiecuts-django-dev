b1f0daeb6eb9982fd1d9fdfe406403da1b161b4a
GjoeKariuki <githaigageorge12@gmail.com>
2024-06-05 16:14:59 +0300

    first commit

---
+++ cookie_cuts/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cookie_cuts.users.models import User
+from cookie_cuts.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
